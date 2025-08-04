03f4b9cd7522c583951b81fced5bff64b285041e
권도현 <tobeykwon@gmail.com>
2025-08-04 21:46:44 +0900

    first commit

---
+++ src/main/kotlin/com/dohyundev/ecommerceproject/ECommerceProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.dohyundev.ecommerceproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommerceProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<ECommerceProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/dohyundev/ecommerceproject/ECommerceProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dohyundev.ecommerceproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ECommerceProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
