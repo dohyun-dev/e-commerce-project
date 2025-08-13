d24416257a67fe07b9db71e173aa7022336018d8
dohyundev0425 <kwondh0351@modaoutlet.co.kr>
2025-08-13 16:38:13 +0900

    feat: ShopServiceV1 추가

---
+++ src/main/kotlin/com/dohyundev/ecommerceproject/application/ShopServiceV1.kt
@@ -0,0 +1,17 @@
+package com.dohyundev.ecommerceproject.application
+
+import com.dohyundev.ecommerceproject.domain.shop.Shop
+import com.dohyundev.ecommerceproject.domain.shop.ShopRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class ShopServiceV1(
+    private val shopRepository: ShopRepository,
+) {
+    @Transactional
+    fun create(name: String): Long? {
+        val newShop = Shop(name = name)
+        return shopRepository.save(newShop).id!!
+    }
+}
