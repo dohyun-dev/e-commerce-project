77bfcfb65ad831b84d1b49ec0d3dc2b4e4f12b67
dohyundev0425 <kwondh0351@modaoutlet.co.kr>
2025-08-13 16:38:56 +0900

    feat: ShopApiControllerV1 추가

---
+++ src/main/kotlin/com/dohyundev/ecommerceproject/presentation/api/ShopApiControllerV1.kt
@@ -0,0 +1,27 @@
+package com.dohyundev.ecommerceproject.presentation.api
+
+import com.dohyundev.ecommerceproject.presentation.api.dto.ShopRequest
+import com.dohyundev.ecommerceproject.application.ShopServiceV1
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/shops")
+class ShopApiControllerV1(
+    private val shopService: ShopServiceV1
+) {
+    @PostMapping
+    fun create(
+        @RequestBody request: ShopRequest.Create
+    ): ResponseEntity<Long> {
+        val shopId = shopService.create(request.name)
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(shopId)
+    }
+}
