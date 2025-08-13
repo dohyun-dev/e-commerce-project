610ac413ef82549f754ea08519fd11c50805a9a5
dohyundev0425 <kwondh0351@modaoutlet.co.kr>
2025-08-13 16:46:48 +0900

    feat: Product 도메인 command service 추가

---
+++ src/main/kotlin/com/dohyundev/ecommerceproject/application/ProductVariantServiceV1.kt
@@ -0,0 +1,40 @@
+package com.dohyundev.ecommerceproject.application
+
+import com.dohyundev.ecommerceproject.common.exception.BusinessException
+import com.dohyundev.ecommerceproject.domain.product.*
+import com.dohyundev.ecommerceproject.domain.product.option.ProductOptionRepository
+import com.dohyundev.ecommerceproject.domain.product.variant.ProductVariant
+import com.dohyundev.ecommerceproject.domain.product.variant.ProductVariantRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class ProductVariantServiceV1(
+    val productRepository: ProductRepository,
+    val productOptionRepository: ProductOptionRepository,
+    val productVariantRepository: ProductVariantRepository
+) {
+    @Transactional
+    fun create(
+        productId: Long,
+        sku: String,
+        optionIds: List<Long>
+    ): Long {
+        val product = productRepository.findById(productId)
+            .orElseThrow { ProductNotFoundException() }
+
+        val productOptions = productOptionRepository.findAllById(optionIds)
+
+        if (optionIds.size != productOptions.size) {
+            throw BusinessException("옵션 정보가 존재하지 않습니다.")
+        }
+
+        val productVariant = ProductVariant.create(
+            product = product,
+            sku = sku,
+            options = productOptions
+        )
+
+        return productVariantRepository.save(productVariant).id!!
+    }
+}
