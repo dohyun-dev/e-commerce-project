651324ccb5b32a9095e0ac63d4caebd51586ffa0
dohyundev0425 <kwondh0351@modaoutlet.co.kr>
2025-08-14 15:52:29 +0900

    fix: 리액트 라우터 스프링 컨트롤러 경로 충돌 문제 해결

--- src/main/kotlin/com/dohyundev/ecommerceproject/presentation/config/CorsConfig.kt
+++ src/main/kotlin/com/dohyundev/ecommerceproject/presentation/config/CorsConfig.kt
@@ -1,17 +1,16 @@
-package com.dohyundev.ecommerceproject.presentation.config;
+package com.dohyundev.ecommerceproject.presentation.config
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
     }
 }
